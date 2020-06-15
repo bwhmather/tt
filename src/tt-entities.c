cd38214f872f3060281187b0de5b483ae197343b
Ben Mather <bwhmather@bwhmather.com>
2020-06-15 22:28:12 +0100

    Properly implement resizing of entity id free list

--- src/tt-entities.c
+++ src/tt-entities.c
@@ -5,14 +5,18 @@
 #include <assert.h>
 
 static size_t tt_entities_max = 0;
-static size_t tt_entities_num_free = 0;
+static size_t tt_entities_free_list_length = 0;
+static size_t tt_entities_free_list_capacity = 0;
 static TTEntityId *tt_entities_free_list = NULL;
 
 
 void tt_entities_startup(void) {
     assert(tt_entities_free_list == NULL);
     
-    tt_entities_free_list = (TTEntityId *) calloc(sizeof(TTEntityId), 1024);
+    tt_entities_free_list_capacity = 256;
+    tt_entities_free_list = (TTEntityId *) calloc(
+        sizeof(TTEntityId), tt_entities_free_list_capacity
+    );
 }
 
 void tt_entities_shutdown(void) {
@@ -22,9 +26,11 @@ void tt_entities_shutdown(void) {
 }
 
 TTEntityId tt_entities_new_id(void) {
-    if (tt_entities_num_free) {
-        tt_entities_num_free--;
-        return tt_entities_free_list[tt_entities_num_free];
+    assert(tt_entities_free_list != NULL);
+
+    if (tt_entities_free_list_length) {
+        tt_entities_free_list_length--;
+        return tt_entities_free_list[tt_entities_free_list_length];
     } else {
         tt_entities_max++;
         return tt_entities_max;
@@ -32,7 +38,20 @@ TTEntityId tt_entities_new_id(void) {
 }
 
 void tt_entities_release_id(TTEntityId entity_id) {
-    assert(tt_entities_num_free < 1024);  // TODO realloc.
-    tt_entities_free_list[tt_entities_num_free] = entity_id;
-    tt_entities_num_free++;
+    assert(tt_entities_free_list != NULL);
+
+    assert(entity_id != 0);
+    assert(entity_id <= tt_entities_max);
+
+    if (tt_entities_free_list_length == tt_entities_free_list_capacity) {
+        tt_entities_free_list_capacity += tt_entities_free_list_capacity / 2;
+        tt_entities_free_list = realloc(
+            tt_entities_free_list,
+            tt_entities_free_list_capacity * sizeof(TTEntityId)
+        );
+        assert(tt_entities_free_list != NULL);
+    }
+
+    tt_entities_free_list[tt_entities_free_list_length] = entity_id;
+    tt_entities_free_list_length++;
 }
---
+++ tests/entities/test_realloc_free_list.c
@@ -0,0 +1,28 @@
+/**
+ * Checks that free list will continue to work when it contains more than its
+ * initial capacity.
+ */
+#include "tt-entities.h"
+
+#include "tt-test.h"
+
+
+int main(void) {
+    tt_entities_startup();
+
+    for (size_t i = 1; i < 10001; i++) {
+        tt_assert(tt_entities_new_id() == i);
+    }
+
+    for (size_t i = 10000; i > 0; i--) {
+        tt_entities_release_id(i);
+    }
+
+    for (size_t i = 1; i > 10001; i--) {
+        tt_assert(tt_entities_new_id() == i);
+    }
+
+    tt_entities_shutdown();
+
+    return 0;
+}
