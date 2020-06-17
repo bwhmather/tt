2f8d6ff83140facfb788ed52f88abb7cfbb6ea32
Ben Mather <bwhmather@bwhmather.com>
2020-06-17 19:56:28 +0100

    Add test for removing things from vector storage

---
+++ tests/tt-test-storage-vector-removal.c
@@ -0,0 +1,30 @@
+/**
+ * Checks that things can be removed from a storage vector.
+ */
+#include "tt-entities.h"
+#include "tt-storage-vector.h"
+
+#include "tt-test.h"
+
+#include <assert.h>
+#include <stdint.h>
+
+
+int main(void) {
+    tt_entities_startup();
+
+    TTStorageVector *storage = tt_storage_vector_new(sizeof(uint64_t));
+
+    TTEntityId id = tt_entities_new_id();
+
+    assert(tt_storage_vector_get(storage, id) == NULL);
+    tt_storage_vector_add(storage, id);
+    assert(tt_storage_vector_get(storage, id) != NULL);
+    tt_storage_vector_remove(storage, id);
+    assert(tt_storage_vector_get(storage, id) == NULL);
+
+    tt_storage_vector_free(storage);
+    tt_entities_shutdown();
+
+    return 0;
+}
