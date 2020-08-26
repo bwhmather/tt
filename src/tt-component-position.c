b237b43bc706987f362e9dc1d5f4495bdf38bc64
Ben Mather <bwhmather@bwhmather.com>
2020-08-26 22:30:13 +0100

    Remove components from entities before release

--- src/tt-component-position.c
+++ src/tt-component-position.c
@@ -6,12 +6,17 @@
 
 
 static TTStorageVector *tt_component_position_storage = NULL;
+static int tt_component_position_release_handle = 0;
 
 
 void tt_component_position_startup(void) {
     assert(tt_component_position_storage == NULL);
 
     tt_component_position_storage = tt_storage_vector_new(sizeof(TTPosition));
+
+    tt_component_position_release_handle = tt_entities_bind_release_callback(
+        tt_component_position_remove
+    );
 }
 
 void tt_component_position_shutdown(void) {
@@ -19,6 +24,9 @@ void tt_component_position_shutdown(void) {
 
     tt_storage_vector_free(tt_component_position_storage);
     tt_component_position_storage = NULL;
+
+    tt_entities_unbind_release_callback(tt_component_position_release_handle);
+    tt_component_position_release_handle = 0;
 }
 
 TTPosition *tt_component_position_add(TTEntityId entity) {
--- src/tt-component-sprite.c
+++ src/tt-component-sprite.c
@@ -6,12 +6,17 @@
 
 
 static TTStorageVector *tt_component_sprite_storage = NULL;
+static int tt_component_sprite_release_handle = 0;
 
 
 void tt_component_sprite_startup(void) {
     assert(tt_component_sprite_storage == NULL);
 
     tt_component_sprite_storage = tt_storage_vector_new(sizeof(TTSprite));
+
+    tt_component_sprite_release_handle = tt_entities_bind_release_callback(
+        tt_component_sprite_remove
+    );
 }
 
 void tt_component_sprite_shutdown(void) {
@@ -19,6 +24,9 @@ void tt_component_sprite_shutdown(void) {
 
     tt_storage_vector_free(tt_component_sprite_storage);
     tt_component_sprite_storage = NULL;
+
+    tt_entities_unbind_release_callback(tt_component_sprite_release_handle);
+    tt_component_sprite_release_handle = 0;
 }
 
 TTSprite *tt_component_sprite_add(TTEntityId entity) {
