6442eac08c20511d93652d9d8af75a3c493258e6
Ben Mather <bwhmather@bwhmather.com>
2020-08-26 22:51:53 +0100

    Rename entity-set to storage-bitset

---
+++ include/tt-storage-bitset.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "tt-entities.h"
+
+#include <stdbool.h>
+
+typedef struct TTStorageBitSet TTStorageBitSet;
+
+TTStorageBitSet *tt_storage_bitset_new(void);
+void tt_storage_bitset_free(TTStorageBitSet* entity_set);
+
+void tt_storage_bitset_add(TTStorageBitSet *entity_set, TTEntityId entity);
+void tt_storage_bitset_remove(TTStorageBitSet *entity_set, TTEntityId entity);
+bool tt_storage_bitset_contains(TTStorageBitSet *entity_set, TTEntityId entity);
+
---
+++ src/tt-storage-bitset.c
@@ -0,0 +1,101 @@
+#include "tt-storage-bitset.h"
+
+#include "tt-entities.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct TTStorageBitSet {
+    size_t nblocks;
+    uint64_t *mask;
+};
+
+TTStorageBitSet *tt_storage_bitset_new(void) {
+    size_t nblocks = 8;
+
+    TTStorageBitSet *storage_bitset = malloc(sizeof(TTStorageBitSet));
+    assert(storage_bitset != NULL);
+
+    storage_bitset->mask = (uint64_t *) malloc(sizeof(uint64_t) * nblocks);
+    assert(storage_bitset->mask != NULL);
+    memset(storage_bitset->mask, 0, sizeof(uint64_t) * nblocks);
+
+    storage_bitset->nblocks = nblocks;
+
+    return storage_bitset;
+}
+
+void tt_storage_bitset_free(TTStorageBitSet* storage_bitset) {
+    assert(storage_bitset != NULL);
+    free(storage_bitset);
+}
+
+void tt_storage_bitset_add(
+    TTStorageBitSet *storage_bitset, TTEntityId entity_id
+) {
+    assert(storage_bitset != NULL);
+
+    size_t block = entity_id / 64;
+    int bit = entity_id % 64;
+
+    if (block >= storage_bitset->nblocks) {
+        size_t old_nblocks = storage_bitset->nblocks;
+        size_t new_nblocks = storage_bitset->nblocks;
+
+        while (block >= new_nblocks) {
+            new_nblocks += new_nblocks / 2;
+        }
+
+        /* Reallocate mask. */
+        storage_bitset->mask = (uint64_t *) realloc(
+            storage_bitset->mask,
+            sizeof(TTStorageBitSet) +
+            sizeof(uint64_t) * new_nblocks
+        );
+        assert(storage_bitset->mask);
+        memset(
+            &storage_bitset->mask[old_nblocks],
+            0, sizeof(uint64_t) * (new_nblocks - old_nblocks)
+        );
+    }
+
+    storage_bitset->mask[block] |= 1 << bit;
+}
+
+void tt_storage_bitset_remove(
+    TTStorageBitSet *storage_bitset, TTEntityId entity_id
+) {
+    assert(storage_bitset != NULL);
+
+    if (!tt_storage_bitset_contains(storage_bitset, entity_id)) {
+        return;
+    }
+
+    size_t block = entity_id / 64;
+    int bit = entity_id % 64;
+
+    storage_bitset->mask[block] &= ~(1 << bit);
+}
+
+bool tt_storage_bitset_contains(
+    TTStorageBitSet *storage_bitset, TTEntityId entity_id
+) {
+    assert(storage_bitset != NULL);
+
+    size_t block = entity_id / 64;
+    int bit = entity_id % 64;
+
+    if (block >= storage_bitset->nblocks) {
+        return false;
+    }
+
+    if (!(storage_bitset->mask[block] & (1 << bit))) {
+        return false;
+    }
+
+    return true;
+}
+
