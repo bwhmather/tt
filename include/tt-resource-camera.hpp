dc91a9935da0b0587a9a836016c0b93558c72e24
Ben Mather <bwhmather@bwhmather.com>
2021-04-30 19:58:35 +0100

    Add shared camera resource

---
+++ include/tt-resource-camera.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+
+void tt_resource_camera_startup();
+void tt_resource_camera_shutdown();
+
+void tt_camera_set_fov(float fov);
+void tt_camera_set_aspect_ratio(float aspect_ratio);
+void tt_camera_set_near_clipping_plane(float near);
+void tt_camera_set_far_clipping_plane(float far);
+
+void tt_camera_look_at(
+    glm::vec3 eye_vector, glm::vec3 centre_vector, glm::vec3 up_vector
+);
+
+glm::mat4 tt_camera_get_matrix(void);
+
---
+++ src/tt-resource-camera.cpp
@@ -0,0 +1,75 @@
+#include "tt-resource-camera.hpp"
+
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/scalar_constants.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+
+
+namespace detail {
+
+static float fov;
+static float aspect_ratio;
+
+static float near_clipping_plane;
+static float far_clipping_plane;
+
+static glm::vec3 eye_vector;
+static glm::vec3 centre_vector;
+static glm::vec3 up_vector;
+
+} /* namespace detail */
+
+void tt_resource_camera_startup() {
+    detail::fov = glm::pi<float>() / 3.0f;
+    detail::aspect_ratio = 1.0f;
+
+    detail::near_clipping_plane = 1.0f;
+    detail::far_clipping_plane = 100.0f;
+
+    detail::eye_vector = glm::vec3(0.0f, -1.0f, 0.0f);
+    detail::centre_vector = glm::vec3(0.0f, 0.0f, 0.0f);
+    detail::up_vector = glm::vec3(0.0, 0.0, 1.0f);
+}
+
+void tt_resource_camera_shutdown() {}
+
+void tt_camera_set_fov(float fov) {
+    detail::fov = fov;
+}
+
+void tt_camera_set_aspect_ratio(float aspect_ratio) {
+    detail::aspect_ratio = aspect_ratio;
+}
+
+void tt_camera_set_near_clipping_plane(float near) {
+    detail::near_clipping_plane = near;
+}
+
+void tt_camera_set_far_clipping_plane(float far) {
+    detail::far_clipping_plane = far;
+}
+
+void tt_camera_look_at(
+    glm::vec3 eye_vector, glm::vec3 centre_vector, glm::vec3 up_vector
+) {
+    detail::eye_vector = eye_vector;
+    detail::centre_vector = centre_vector;
+    detail::up_vector = up_vector;
+}
+
+glm::mat4 tt_camera_get_matrix(void) {
+    glm::mat4 model_matrix = glm::identity<glm::mat4>();
+
+    glm::mat4 view_matrix = glm::lookAt(
+        detail::eye_vector, detail::centre_vector, detail::up_vector
+    );
+
+    glm::mat4 projection_matrix = glm::perspective(
+        detail::fov, detail::aspect_ratio,
+        detail::near_clipping_plane, detail::far_clipping_plane
+    );
+
+    return projection_matrix * view_matrix * model_matrix;
+}
