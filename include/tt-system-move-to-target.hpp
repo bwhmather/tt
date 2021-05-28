d1d0e6e5e1bd3d5415a9cf60168933897a0a4b68
Ben Mather <bwhmather@bwhmather.com>
2021-05-28 22:10:03 +0100

    Remove obsolete move to target and goal systems and components

--- include/tt-system-move-to-target.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-extern "C" {
-#include "tt-entities.h"
-}
-
-void tt_system_move_to_target_startup(void);
-void tt_system_move_to_target_shutdown(void);
-
-void tt_system_move_to_target_run(void);
