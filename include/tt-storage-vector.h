d6ecd25364621698e263aa70a9351230baaa3136
Ben Mather <bwhmather@bwhmather.com>
2021-06-11 22:00:54 +0100

    Remove deleted entities from vector storage

--- include/tt-storage-vector.h
+++ include/tt-storage-vector.h
@@ -7,6 +7,7 @@ typedef struct TTStorageVector TTStorageVector;
 TTStorageVector *tt_storage_vector_new(size_t component_size);
 void tt_storage_vector_free(TTStorageVector *storage);
 
+bool tt_storage_vector_has(TTStorageVector *storage, TTEntityId entity_id);
 void *tt_storage_vector_add(TTStorageVector *storage, TTEntityId entity);
 void *tt_storage_vector_get(TTStorageVector *storage, TTEntityId entity);
 void tt_storage_vector_remove(TTStorageVector *storage, TTEntityId entity);
--- src/tt-storage-vector.c
+++ src/tt-storage-vector.c
@@ -13,12 +13,24 @@ struct TTStorageVector {
     size_t count;
     uint64_t *mask;
     unsigned char *buffer;
+    int delete_callback_handle;
 };
+
+
+static void on_delete_callback(TTEntityId entity_id, void *data) {
+    TTStorageVector *storage = (TTStorageVector *) data;
+
+    if (tt_storage_vector_has(storage, entity_id)) {
+        tt_storage_vector_remove(storage, entity_id);
+    }
+}
+
 
 TTStorageVector *tt_storage_vector_new(size_t component_size) {
     TTStorageVector *storage = (TTStorageVector *) malloc(
         sizeof(TTStorageVector)
     );
+    tt_assert(storage != NULL);
 
     storage->size = component_size;
     storage->count = 256;
@@ -26,19 +38,31 @@ TTStorageVector *tt_storage_vector_new(size_t component_size) {
     storage->mask = (uint64_t *) calloc(
         sizeof(uint64_t), ((storage->count - 1) / 64) + 1
     );
+    tt_assert(storage->mask != NULL);
+
     storage->buffer = (unsigned char *) calloc(component_size, storage->count);
+    tt_assert(storage->buffer != NULL);
+
+
+    storage->delete_callback_handle = tt_entities_bind_on_delete_callback(
+        &on_delete_callback, storage
+    );
 
     return storage;
 }
 
+
 void tt_storage_vector_free(TTStorageVector *storage) {
     tt_assert(storage != NULL);
+
+    tt_entities_unbind_on_delete_callback(storage->delete_callback_handle);
 
     free(storage->mask);
     free(storage->buffer);
 
     free(storage);
 }
+
 
 bool tt_storage_vector_has(TTStorageVector *storage, TTEntityId entity_id) {
     tt_assert(storage != NULL);
@@ -53,6 +77,7 @@ bool tt_storage_vector_has(TTStorageVector *storage, TTEntityId entity_id) {
 
     return true;
 }
+
 
 void *tt_storage_vector_add(TTStorageVector *storage, TTEntityId entity_id) {
     tt_assert(storage != NULL);
@@ -93,6 +118,7 @@ void *tt_storage_vector_add(TTStorageVector *storage, TTEntityId entity_id) {
     return (void *) &storage->buffer[storage->size * entity_id];
 }
 
+
 void *tt_storage_vector_get(TTStorageVector *storage, TTEntityId entity_id) {
     tt_assert(storage != NULL);
 
@@ -105,6 +131,7 @@ void *tt_storage_vector_get(TTStorageVector *storage, TTEntityId entity_id) {
 
     return &storage->buffer[storage->size * entity_id];
 }
+
 
 void tt_storage_vector_remove(TTStorageVector *storage, TTEntityId entity_id) {
     tt_assert(storage != NULL);
