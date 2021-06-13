3e6abdc62e0e36b3ecf8af69b7eec5d8635ca5d3
Ben Mather <bwhmather@bwhmather.com>
2021-06-13 13:08:33 +0100

    Fix up select tree behaviour

--- src/tt-behaviour-select-tree.c
+++ src/tt-behaviour-select-tree.c
@@ -1,57 +1,77 @@
 #include "tt-behaviour-select-tree.h"
 
 #include <malloc.h>
-#include <stddef.h>
+#include <math.h>
 
+#include "bt.h"
 #include "tt-behaviour.h"
+#include "tt-component-harvestable.h"
+#include "tt-component-position.h"
+#include "tt-component-target.h"
+#include "tt-component-wood.h"
+#include "tt-entities.h"
 #include "tt-error.h"
+#include "tt-utils.h"
 
 
-static TTBehaviourResult tt_behaviour_select_tree_do_call(
-    TTBehaviour *behaviour, TTEntityId entity_id, void *fp
+static BTResult tt_behaviour_select_tree_tick(
+    BTBehaviour *behaviour,
+    void *state,
+    TTBehaviourContext *context
 ) {
-    // TODO
-    return TT_BEHAVIOUR_SUCCEEDED;
-}
-
-static TTBehaviourResult tt_behaviour_select_tree_do_resume(
-    TTBehaviour *behaviour, TTEntityId entity_id, void *fp
-) {
-    // TODO
-    tt_assert(false);
-}
-
-static void tt_behaviour_select_tree_do_interrupt(
-    TTBehaviour *behaviour, TTEntityId entity_id, void *fp) {
-    return;
-}
-
-
-static size_t tt_behaviour_select_tree_max_stack_size(TTBehaviour *behaviour) {
     (void) behaviour;
-    return 0;
+    (void) state;
+
+    TTEntityId entity_id = context->entity_id;
+    TTEntityIter iter;
+
+    tt_entities_iter_begin(&iter);
+
+    TTEntityId target_id = 0;
+    double target_distance = INFINITY;
+
+    while (tt_entities_iter_has_next(&iter)) {
+        TTEntityId candidate_id = tt_entities_iter_next(&iter);
+
+        if (!tt_component_harvestable_get(candidate_id)) continue;
+        if (!tt_component_wood_has(candidate_id)) continue;
+        if (!tt_component_position_has(candidate_id)) continue;
+
+        double candidate_distance = tt_entity_distance(entity_id, candidate_id);
+
+        if (candidate_distance < target_distance) {
+            target_id = candidate_id;
+            target_distance = candidate_distance;
+        }
+    }
+
+    if (target_id) {
+        tt_component_target_set(entity_id, target_id);
+        return BT_SUCCEEDED;
+    } else {
+        return BT_FAILED;
+    }
 }
 
 
-static void tt_behaviour_select_tree_free(TTBehaviour *behaviour) {
+static void tt_behaviour_select_tree_free(BTBehaviour *behaviour) {
     free(behaviour);
 }
 
 
-TTBehaviour *tt_behaviour_select_tree(void) {
-    TTBehaviour *behaviour = (TTBehaviour *) malloc(sizeof(TTBehaviour));
+BTBehaviour *tt_behaviour_select_tree(void) {
+    BTBehaviour *behaviour = (BTBehaviour *) malloc(sizeof(BTBehaviour));
     tt_assert(behaviour != NULL);
-    *behaviour = (TTBehaviour) {
-        .do_call = tt_behaviour_select_tree_do_call,
-        .do_resume = tt_behaviour_select_tree_do_resume,
-        .do_interrupt = tt_behaviour_select_tree_do_interrupt,
+
+    *behaviour = (BTBehaviour) {
+        .init = NULL,
+        .tick = (BTTickFn) tt_behaviour_select_tree_tick,
+        .interrupt = NULL,
 
         .frame_size = 0,
-        .compute_max_stack_size = tt_behaviour_select_tree_max_stack_size,
 
         .free = tt_behaviour_select_tree_free
     };
 
     return behaviour;
 }
-
