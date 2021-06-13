5fbd9a0f5586e6fee754f3b582bbb10074cc7abc
Ben Mather <bwhmather@bwhmather.com>
2021-06-13 12:35:12 +0100

    Fix utils module

--- include/tt-utils.h
+++ include/tt-utils.h
@@ -1,7 +1,5 @@
 #pragma once
 
-extern "C" {
 #include "tt-entities.h"
-}
 
 double tt_entity_distance(TTEntityId a, TTEntityId b);
---
+++ src/tt-utils.c
@@ -0,0 +1,21 @@
+#include "tt-utils.h"
+
+#include <math.h>
+
+#include "tt-component-position.h"
+#include "tt-entities.h"
+
+
+double tt_entity_distance(TTEntityId a, TTEntityId b) {
+    if (!tt_component_position_has(a) || !tt_component_position_has(b)) {
+        return INFINITY;
+    }
+
+    TTPosition *pos_a = tt_component_position_get(a);
+    TTPosition *pos_b = tt_component_position_get(b);
+
+    return sqrt(
+        pow(pos_a->x - pos_b->x, 2) +
+        pow(pos_a->y - pos_b->y, 2)
+    );
+}
